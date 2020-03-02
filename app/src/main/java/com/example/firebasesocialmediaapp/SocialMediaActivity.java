65fc101e010b22573fbcb7ee09bb64eb4855a7ad
ledungcobra <ledungcobra@gmail.com>
2020-03-03 00:23:56 +0700

    Upload image to server

--- app/src/main/java/com/example/firebasesocialmediaapp/SocialMediaActivity.java
+++ app/src/main/java/com/example/firebasesocialmediaapp/SocialMediaActivity.java
@@ -1,46 +1,107 @@
 package com.example.firebasesocialmediaapp;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.shashank.sony.fancytoastlib.FancyToast;
 
-public class SocialMediaActivity extends AppCompatActivity {
+import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class SocialMediaActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
 
     private FirebaseAuth mAuth;
-    private ImageView imgView;
+    private ImageView postImageView;
     private Button btnCreatePost;
-    private EditText edtDesciption;
+    private EditText edtDescription;
     private ListView usersListView;
+    private Bitmap receivedBitMap;
 
+    private String imageIdentifier;
+    private ArrayAdapter adapter;
+    private ArrayList<String> usernames;
+    private ArrayList<String> uids;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_social_media);
+
         mAuth = FirebaseAuth.getInstance();
+
+        postImageView = findViewById(R.id.postImageView);
+        btnCreatePost = findViewById(R.id.btnCreatePost);
+        edtDescription = findViewById(R.id.edtDescription);
+        usersListView = findViewById(R.id.usersListView);
+
+        btnCreatePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImageToServer();
+            }
+        });
+        postImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                requestPermissionForReadExternalStorage();
+
+            }
+        });
+
+        usernames = new ArrayList<>();
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, usernames);
+        usersListView.setAdapter(adapter);
+        uids = new ArrayList<>();
+
+        usersListView.setOnItemClickListener(this);
 
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.my_menu,menu);
+        getMenuInflater().inflate(R.menu.my_menu, menu);
 
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(item.getItemId()== R.id.logOutItem){
+        if (item.getItemId() == R.id.logOutItem) {
 
             logOut();
 
@@ -48,12 +109,159 @@ public class SocialMediaActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    private void logOut(){
+    private void logOut() {
 
         mAuth.signOut();
         finish();
-        startActivity(new Intent(this,MainActivity.class));
+        startActivity(new Intent(this, MainActivity.class));
 
+
+    }
+
+    private void requestPermissionForReadExternalStorage() {
+
+        if (Build.VERSION.SDK_INT >= 23 &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+
+            requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1000);
+
+        } else {
+
+            getChosenImage();
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1000 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            getChosenImage();
+        } else {
+
+
+        }
+
+    }
+
+    private void getChosenImage() {
+
+        startActivityForResult(new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI), 2000);
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 2000) {
+
+            if (resultCode == RESULT_OK) {
+
+                try {
+
+                    Uri seltectedImage = data.getData();
+                    receivedBitMap = BitmapFactory.decodeStream(getContentResolver().openInputStream(seltectedImage));
+                    postImageView.setImageBitmap(receivedBitMap);
+
+
+                } catch (Exception e) {
+
+                    showToast("Failed", FancyToast.ERROR);
+
+                }
+
+            }
+
+        }
+    }
+
+    private void showToast(String message, int type) {
+
+        FancyToast.makeText(this, message, FancyToast.LENGTH_SHORT, type, false).show();
+
+    }
+
+    private void uploadImageToServer() {
+        // Create a storage reference from our app
+
+        if (postImageView != null) {
+
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference();
+            imageIdentifier = UUID.randomUUID() + ".png";
+
+
+// Create a reference to "mountains.jpg"
+            StorageReference mountainsRef = storageRef.child("image/mountains.jpg");
+            // Get the data from an ImageView as bytes
+            postImageView.setDrawingCacheEnabled(true);
+            postImageView.buildDrawingCache();
+            Bitmap bitmap = ((BitmapDrawable) postImageView.getDrawable()).getBitmap();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
+            byte[] data = baos.toByteArray();
+
+            UploadTask uploadTask = mountainsRef.putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception) {
+                    // Handle unsuccessful uploads
+                    showToast("Failed upload", FancyToast.ERROR);
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    // taskSnapshot.getMetadata() contains file metadata such as size, content-type, etc.
+                    // ...
+                    showToast("Success upload file", FancyToast.SUCCESS);
+                    edtDescription.setVisibility(View.VISIBLE);
+                    edtDescription.setHint("Please enter your description here");
+
+                    FirebaseDatabase.getInstance().getReference().child("my_users").addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                            uids.add(dataSnapshot.getKey());
+                            String username = dataSnapshot.child("username").getValue()+"";
+                            usernames.add(username);
+                            adapter.notifyDataSetChanged();
+
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                        }
+                    });
+
+                }
+            });
+
+
+        }
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
     }
 }
